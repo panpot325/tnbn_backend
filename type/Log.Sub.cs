5b13443fd6acd513e94796b904cf3f6b65f14755
kyopen <kyopen@asahinet.jp>
2024-12-31 16:41:09 +0900

    Sub_LogWrite

---
+++ type/Log.Sub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using BackendMonitor.Properties;
+
+namespace BackendMonitor.type;
+
+/// <summary>
+/// Logクラス
+/// </summary>
+public partial class Log {
+    /// <summary>
+    /// Sub_LogWrite
+    /// </summary>
+    /// <param name="message"></param>
+    public static void Sub_LogWrite(string message) {
+        if (Settings.Default.Log_Write != 1) return;
+        using var sw = new StreamWriter(Settings.Default.Log_File_Path, true, Encoding.UTF8);
+        sw.WriteLine($"{DateTime.Now:yyyy/MM/dd HH:mm:ss}\t{message}");
+    }
+}
