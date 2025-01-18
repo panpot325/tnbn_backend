3af2700750998cd2ae238b7c7dda88fe7198fde9
kyopen <kyopen@asahinet.jp>
2025-01-18 10:56:09 +0900

    フォームデザイン調整

--- Form1.Component.cs
+++ Form1.Component.cs
@@ -3,6 +3,7 @@
 using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
+using BackendMonitor.share;
 using BackendMonitor.type.singleton;
 using Timer = System.Timers.Timer;
 
@@ -58,8 +59,8 @@ public partial class Form1 {
         // 
         Text1.Name = @"Text1";
         Text1.Text = "";
-        Text1.Location = new Point(48, 214);
-        Text1.Size = new Size(680, 100);
+        Text1.Location = new Point(20, 100);
+        Text1.Size = new Size(400, 80);
         Text1.TabIndex = 1;
         Text1.Font = new Font("ＭＳ Ｐゴシック", 10.875F, FontStyle.Bold,
             GraphicsUnit.Point, 128);
@@ -69,8 +70,8 @@ public partial class Form1 {
         Command1.Name = @"Command1";
         Command1.Text = @"終了";
         Command1.Enabled = false;
-        Command1.Location = new Point(48, 28);
-        Command1.Size = new Size(288, 99);
+        Command1.Location = new Point(30, 20);
+        Command1.Size = new Size(150, 50);
         Command1.TabIndex = 0;
         Command1.UseVisualStyleBackColor = true;
         Command1.Click += Command1_Click;
@@ -79,8 +80,8 @@ public partial class Form1 {
         // 
         button1.Name = @"button1";
         button1.Text = @"テスト";
-        button1.Location = new Point(409, 28);
-        button1.Size = new Size(288, 99);
+        button1.Location = new Point(200, 20);
+        button1.Size = new Size(150, 50);
         button1.TabIndex = 2;
         button1.UseVisualStyleBackColor = true;
         button1.Click += button1_Click_1;
@@ -89,8 +90,8 @@ public partial class Form1 {
         // 
         button2.Name = @"button2";
         button2.Text = @"船番一覧要求";
-        button2.Location = new Point(48, 353);
-        button2.Size = new Size(288, 109);
+        button2.Location = new Point(30, 180);
+        button2.Size = new Size(150, 50);
         button2.TabIndex = 3;
         button2.UseVisualStyleBackColor = true;
         button2.Click += button2_Click;
@@ -99,8 +100,8 @@ public partial class Form1 {
         // 
         button3.Name = @"button3";
         button3.Text = @"ブロック名一覧要求";
-        button3.Location = new Point(409, 362);
-        button3.Size = new Size(288, 100);
+        button3.Location = new Point(200, 180);
+        button3.Size = new Size(150, 50);
         button3.TabIndex = 4;
         button3.UseVisualStyleBackColor = true;
         button3.Click += button3_Click;
@@ -109,8 +110,8 @@ public partial class Form1 {
         // 
         button4.Name = @"button4";
         button4.Text = @"部材名一覧要求";
-        button4.Location = new Point(48, 497);
-        button4.Size = new Size(288, 100);
+        button4.Location = new Point(30, 240);
+        button4.Size = new Size(150, 50);
         button4.TabIndex = 5;
         button4.UseVisualStyleBackColor = true;
         button4.Click += button4_Click;
@@ -119,8 +120,8 @@ public partial class Form1 {
         // 
         button5.Name = @"button5";
         button5.Text = @"ワークデータ要求";
-        button5.Location = new Point(409, 497);
-        button5.Size = new Size(287, 100);
+        button5.Location = new Point(200, 240);
+        button5.Size = new Size(150, 50);
         button5.TabIndex = 6;
         button5.UseVisualStyleBackColor = true;
         button5.Click += button5_Click;
@@ -129,8 +130,8 @@ public partial class Form1 {
         // 
         button6.Name = @"button6";
         button6.Text = @"稼働開始";
-        button6.Location = new Point(48, 632);
-        button6.Size = new Size(287, 100);
+        button6.Location = new Point(30, 300);
+        button6.Size = new Size(150, 50);
         button6.TabIndex = 6;
         button6.UseVisualStyleBackColor = true;
         button6.Click += button6_Click;
@@ -139,8 +140,8 @@ public partial class Form1 {
         // 
         button7.Name = @"button6";
         button7.Text = @"稼働終了";
-        button7.Location = new Point(409, 632);
-        button7.Size = new Size(287, 100);
+        button7.Location = new Point(200, 300);
+        button7.Size = new Size(150, 50);
         button7.TabIndex = 6;
         button7.UseVisualStyleBackColor = true;
         button7.Click += button7_Click;
@@ -149,19 +150,23 @@ public partial class Form1 {
         // 
         Name = @"Form1";
         Text = @"Form1";
-        AutoScaleDimensions = new SizeF(13F, 24F);
-        AutoScaleMode = AutoScaleMode.Font;
-        Size = new Size(500, 600);
-        ClientSize = new Size(1161, 966);
+        //AutoScaleDimensions = new SizeF(13F, 24F);
+        //AutoScaleMode = AutoScaleMode.None;
+        //Size = new Size(800, 600);
+        ClientSize = new Size(380, 200);
         Controls.Add(Text1);
         Controls.Add(Command1);
-        Controls.Add(button1);
-        Controls.Add(button2);
-        Controls.Add(button3);
-        Controls.Add(button4);
-        Controls.Add(button5);
-        Controls.Add(button6);
-        Controls.Add(button7);
+        if (AppConfig.debugMode) {
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Controls.Add(button3);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button7);
+            ClientSize = new Size(380, 400);
+        }
+
         ControlBox = false;
 
         Load += Form1_Load;
